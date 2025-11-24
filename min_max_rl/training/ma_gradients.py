682ab4a13546a68a75a65d085c1b49da299a40fc
Tassos Manganaris <tassos.manganaris@gmail.com>
2025-11-24 09:17:03 -0500

    Fix mistake in cgo update formulation

--- min_max_rl/training/ma_gradients.py
+++ min_max_rl/training/ma_gradients.py
@@ -8,7 +8,7 @@ import functools
 
 from jax.tree_util import tree_map, tree_structure, tree_flatten, tree_unflatten
 from jax.flatten_util import ravel_pytree
-from .utils import conjugate_gradient, cross_hvp
+from .utils import cross_hvp
 
 
 def loss_and_pgrad(
@@ -147,62 +147,78 @@ cgd_init_state = cgo_init_state
 
 
 def solve_for_cgo_update(
-    f: Callable[[any, any], jnp.ndarray],
-    x: any,
-    y: any,
-    grad_x: any,
-    grad_y: any,
-    hessian_xy_mult_grad_y: any,
-    alpha: float,
-    nsteps: int = 100,
-    init: any = None,
-    for_x: bool = True,
-    residual_tol: float = 1e-12,
+        f: Callable[[any, any], jnp.ndarray],
+        x: any,
+        y: any,
+        grad_a: any,
+        grad_b: any,
+        hessian_ab_mult_grad_b: any,
+        b_vec: any,
+        alpha: float,
+        nsteps: int = 100,
+        init: any = None,
+        for_x: bool = True,
+        residual_tol: float = 1e-12,
 ) -> any:
     """
-    Solves the linear system:
-        (I + alpha^2 D_{xy} f D_{yx} f) x_delta = -D_x f - alpha D_{xy} f D_y f
-    and returns x_delta as a pytree.
+    The closed form solution for the CGO update is
+
+    \eta (I + \\alpha^{2} D_{xy} f D_{yx} f)^{-1} (\\nabla_x f + \\alpha D_{xy} f \\nabla_y f)
+    \eta (I + \\alpha^{2} D_{yx} f D_{xy} f)^{-1} (\\nabla_y f + \\alpha D_{yx} f \\nabla_x f)
+
+    with ab = xy or ab = yx.
+
+    Then use conjugate gradient to solve the system
+
+    (I + \\alpha^{2} D_{ab} f D_{ba} f) x = (\\nabla_a f + \\alpha D_{ab} f \\nabla_b f)
+
+    for x. plug/minus \eta is applied after that.
     """
 
-    # compute RHS b = -grad_x - eta * D_xy grad_y
-    b = jax.tree.map(lambda a, b: -a - alpha * b, grad_x, hessian_xy_mult_grad_y)
-    b_vec = tm.Vector(b)
-
-    # define matvec: v -> (I + alpha^2 D_{xy} D_{yx}) v
-    def matvec(v):
+    # define matvec: v -> (I + alpha^2 D_{xy} f D_{yx} f) v
+    if for_x:
+      def matvec(v):
         hvp_1, _ = cross_hvp(f, x, y, v.tree, order="yx")
         hvp_2, _ = cross_hvp(f, x, y, hvp_1, order="xy")
         return v + alpha**2 * tm.Vector(hvp_2)
+    else:
+      def matvec(v):
+        hvp_1, _ = cross_hvp(f, x, y, v.tree, order="xy")
+        hvp_2, _ = cross_hvp(f, x, y, hvp_1, order="yx")
+        return v + alpha**2 * tm.Vector(hvp_2)
 
     # initial CG state
-    xk = tm.Vector(jax.tree.map(jnp.zeros_like, b)) if init is None else tm.Vector(init)
-    rk = tm.Vector(b)
+    xk = tm.Vector(jax.tree.map(jnp.zeros_like, b_vec.tree)) if init is None else tm.Vector(init)
+
+    # r_0 = b - A x_0
+    rk = b_vec - matvec(xk)
+
     pk = rk
     rdotr = rk.dot(rk)
     rdotr_init = rdotr
 
     def cond_fn(state):
-        _, rk, _, rdotr = state
+        _, rk, _, rdotr, iters = state
         return rdotr > residual_tol * rdotr_init
 
     def body_fn(state):
-        xk, rk, pk, rdotr = state
+        xk, rk, pk, rdotr, iters = state
         Apk = matvec(pk)
-        alpha = rdotr / pk.dot(Apk)
-        xk_new = xk + alpha * pk
-        rk_new = rk - alpha * Apk
+        alpha_k = rdotr / pk.dot(Apk)
+        xk_new = xk + alpha_k * pk
+        rk_new = rk - alpha_k * Apk
         new_rdotr = rk_new.dot(rk_new)
         beta = new_rdotr / rdotr
         pk_new = rk_new + beta * pk
-        return xk_new, rk_new, pk_new, new_rdotr
+        return xk_new, rk_new, pk_new, new_rdotr, iters+1
 
     # Run the CG loop with early stopping
-    init_state = (xk, rk, pk, rdotr)
+    init_state = (xk, rk, pk, rdotr, 0)
     final_state = jax.lax.while_loop(cond_fn, body_fn, init_state)
 
-    xk_final, *_ = final_state
-    return xk_final.tree
+    xk_final, _, _, _, iters = final_state
+
+    return xk_final
 
 
 def cgo_update_fn(
@@ -215,89 +231,84 @@ def cgo_update_fn(
     alpha: Optional[float] = None,
     have_aux: bool = False,
 ):
-  """Wrapper of the loss function that apply gradient updates.
+    """Wrapper of the loss function that apply gradient updates.
 
-  Args:
-    loss_fn: The loss function.
-    optimizer: The optimizer to apply gradients.
-    pmap_axis_name: If relevant, the name of the pmap axis to synchronize
-      gradients.
-    have_aux: Whether the loss_fns have auxiliary data.
-    eta: learning rate
-    alpha: bilinear term sensitivity term
+    Args:
+      loss_fn: The loss function.
+      optimizer: The optimizer to apply gradients.
+      pmap_axis_name: If relevant, the name of the pmap axis to synchronize
+        gradients.
+      have_aux: Whether the loss_fns have auxiliary data.
+      eta: learning rate
+      alpha: bilinear term sensitivity term
 
-  Returns:
-    A function that takes the same argument as the loss function plus the
-    optimizer state. The output of this function is the loss, the new parameter,
-    and the new optimizer state.
-  """
+    Returns:
+      A function that takes the same argument as the loss function plus the
+      optimizer state. The output of this function is the loss, the new parameter,
+      and the new optimizer state.
+    """
 
-  agent0_linear_loss_and_pgrad_fn = loss_and_pgrad(
-    agent0_linear_loss_term, pmap_axis_name=pmap_axis_name, has_aux=have_aux
-  )
-  agent1_linear_loss_and_pgrad_fn = loss_and_pgrad(
-    agent1_linear_loss_term, pmap_axis_name=pmap_axis_name, has_aux=have_aux
-  )
-
-  if alpha is None:
-    alpha = eta
-
-  def f(*args, optimizer_state, cgo_state):
-    xy_arg, rest = args[0], args[1:]
-    x, y = xy_arg
-    x_result, grad_x = agent0_linear_loss_and_pgrad_fn(x, *rest)
-    y_result, grad_y = agent1_linear_loss_and_pgrad_fn(y, *rest)
-    bilinear_result = bilinear_loss_term(xy_arg, *rest)
-
-    def diadic_bilinear_loss_term(x, y):
-      return bilinear_loss_term([x, y], *rest)
-
-    # D_xy mult D_y
-    hvp_x, bi_grad_y = cross_hvp(
-      diadic_bilinear_loss_term,
-      x, y, grad_y,
-      order = "xy",
-      has_aux=have_aux,
+    agent0_linear_loss_and_pgrad_fn = loss_and_pgrad(
+        agent0_linear_loss_term, pmap_axis_name=pmap_axis_name, has_aux=have_aux
+    )
+    agent1_linear_loss_and_pgrad_fn = loss_and_pgrad(
+        agent1_linear_loss_term, pmap_axis_name=pmap_axis_name, has_aux=have_aux
     )
 
-    # D_yx mult D_x
-    hvp_y, bi_grad_x = cross_hvp(
-      diadic_bilinear_loss_term,
-      x, y, grad_x,
-      order = "yx",
-      has_aux=have_aux,
-    )
+    if alpha is None:
+        alpha = eta
 
-    def solve_for_x(cgo_state):
-      delta_x = solve_for_cgo_update(diadic_bilinear_loss_term, x, y, grad_x, grad_y, hvp_x,
-                                     alpha, for_x=True)
-      d_yx_mult_delta_x, _ = cross_hvp(diadic_bilinear_loss_term, x, y, delta_x)
-      delta_y = tm.Vector(grad_y) + eta * tm.Vector(d_yx_mult_delta_x)
-      return [delta_x, delta_y.tree], cgo_state._replace(old_y=delta_y.tree)
+    def f(*args, optimizer_state, cgo_state):
+        xy_arg, rest = args[0], args[1:]
+        x, y = xy_arg
+        x_result, grad_x = agent0_linear_loss_and_pgrad_fn(x, y, *rest)
+        y_result, grad_y = agent1_linear_loss_and_pgrad_fn(y, x, *rest)
+        bilinear_result = bilinear_loss_term(xy_arg, *rest)
 
-    def solve_for_y(cgo_state):
-      delta_y = solve_for_cgo_update(diadic_bilinear_loss_term, y, x, grad_y, grad_x, hvp_y,
-                                     alpha, for_x=False)
-      d_xy_mult_delta_y, _ = cross_hvp(diadic_bilinear_loss_term, x, y, delta_y)
-      delta_x = tm.Vector(grad_x) - eta * tm.Vector(d_xy_mult_delta_y)
-      return [delta_x.tree, delta_y], cgo_state._replace(old_x=delta_x.tree)
+        # first, we need a function of two parameters to compute the partial
+        # derivatives with respect to those two parameters.
+        def diadic_bilinear_loss_term(x, y):
+            return bilinear_loss_term([x, y], *rest)
 
-    delta_xy, cgo_state = jax.lax.cond(cgo_state.solve_x, solve_for_x, solve_for_y, cgo_state)
-    cgo_state = cgo_state._replace(solve_x=~cgo_state.solve_x)
+        # D_xy mult D_y
+        hvp_x, _ = cross_hvp(
+            diadic_bilinear_loss_term,
+            x, y, grad_y,
+            order = "xy",
+            has_aux=have_aux,
+        )
 
-    params_update, optimizer_state = optimizer.update(delta_xy, optimizer_state, params=xy_arg)
-    params = optax.apply_updates(xy_arg, params_update)
+        # D_yx mult D_x
+        hvp_y, _ = cross_hvp(
+            diadic_bilinear_loss_term,
+            x, y, grad_x,
+            order = "yx",
+            has_aux=have_aux,
+        )
 
-    if have_aux:
-      x_loss, x_metrics = x_result
-      y_loss, y_metrics = y_result
-      result = (x_loss + y_loss, x_metrics | y_metrics)
-    else:
-      result = (x_result + y_result)
+        b_x = tm.Vector(grad_x) - alpha * tm.Vector(hvp_x)
+        b_y = tm.Vector(grad_y) + alpha * tm.Vector(hvp_y)
 
-    return result, params, optimizer_state
+        # this is negated because optimizer.update applies its own negative.
+        # TODO: is this always true?
+        delta_xy = [
+          (-solve_for_cgo_update(diadic_bilinear_loss_term, x, y, grad_x, grad_y, hvp_x, b_x, alpha, for_x=True)).tree,
+          (solve_for_cgo_update(diadic_bilinear_loss_term, x, y, grad_y, grad_x, hvp_y, b_y, alpha, for_x=False)).tree
+        ]
 
-  return f
+        params_update, optimizer_state = optimizer.update(delta_xy, optimizer_state, params=xy_arg)
+        params = optax.apply_updates(xy_arg, params_update)
+
+        if have_aux:
+            x_loss, x_metrics = x_result
+            y_loss, y_metrics = y_result
+            result = (x_loss + y_loss, x_metrics | y_metrics)
+        else:
+            result = (x_result + y_result)
+
+        return result, params, optimizer_state
+
+    return f
 
 
 cgd_update_fn = functools.partial(cgo_update_fn, alpha=None)
